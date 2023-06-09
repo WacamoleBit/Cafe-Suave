ebd01d906b43d1b7a9211d7f8052592d51c59b01
Manuel Hernandez <mahmaruro@outlook.com>
2023-06-09 00:33:33 -0600

    incluir vista de eliminacion de cuenta

---
+++ resources/views/user/destroy.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.app')
+
+@extends('layout.navbar.authnavbar')
+
+@section('content')
+    <div class="container w-25 border py-4">
+        <form action="/user" method="POST">
+            @method('DELETE')
+            @csrf
+            <h1 class="d-flex gap-2 justify-content-center">¿Elminiar cuenta?</h1>
+            <div class="d-flex gap-2 justify-content-center">
+                <button class="btn btn-primary" value="confirm" name="submitbutton" type="submit">Confirmar</button>
+                <button class="btn btn-danger" value="'cancel" name="submitbutton" type="submit">Cancelar</button>
+            </div>
+        </form>
+    </div>   
+@endsection
