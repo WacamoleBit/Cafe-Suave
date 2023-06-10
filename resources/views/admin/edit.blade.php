e731669b09978001e345cc270817a0bd11a08f2a
Manuel Hernandez <mahmaruro@outlook.com>
2023-06-10 17:13:41 -0600

    agregar vista admin/edit.blade.php

---
+++ resources/views/admin/edit.blade.php
@@ -0,0 +1,33 @@
+@extends('layout.app')
+
+@extends('layout.navbar.adminnavbar')
+
+@section('content')
+    <div class="container w-25 border py-4">
+        <form action="/user" method="POST">
+            @method('PUT')
+            @csrf
+            <h1 class="d-flex gap-2 justify-content-center">Mis datos</h1>
+            <div class="form-group mb-3">
+                <label for="nombres">Nombre(s)</label>
+                <input type="text" name="first_name" id="nombres" value="{{ $user->first_name }}" class="form-control" placeholder="Nombre">
+            </div>
+            <div class="form-group mb-3">
+                <label for="apellidos">Apellido(s)</label>
+                <input type="text" name="last_name" id="apellidos" value="{{ $user->last_name }}" class="form-control" placeholder="Apellido">
+            </div>
+            <div class="form-group mb-3">
+                <label for="correo">Correo electrónico</label>
+                <input type="email" name="email" id="correo" value="{{ $user->email }}" class="form-control" placeholder="Correo electrónico">
+            </div>
+            <div class="form-group mb-3">
+                <label for="usuario">Usuario</label>
+                <input type="text" name="username" id="usuario" value="{{ $user->username }}" class="form-control" placeholder="Usuario">
+            </div>
+
+            <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary">Confirmar</button>
+            </div>
+        </form>
+    </div>   
+@endsection
