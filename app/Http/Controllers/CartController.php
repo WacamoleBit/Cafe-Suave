1b2038b74e74f51a1b95a9255d415652474b461a
Manuel Hernandez <mahmaruro@outlook.com>
2023-06-12 00:11:57 -0600

    liminar venta al stock

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -48,7 +48,7 @@ class CartController extends Controller
             }
         }
 
-        if ($total <= $product->stock) {
+        if ($total < $product->stock) {
             $cart[] = $product;
         }
 
